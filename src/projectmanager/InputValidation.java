4c5f29f04945883abcca2a21e0cf88dced7db190
calpayne <34384159+calpayne@users.noreply.github.com>
2018-10-13 15:51:35 +0100

    Added validateProject method

--- src/projectmanager/InputValidation.java
+++ src/projectmanager/InputValidation.java
@@ -1,5 +1,7 @@
 package projectmanager;
 
+import java.util.Date;
+
 /**
  *
  * @author Cal Payne
@@ -25,6 +27,39 @@ public class InputValidation {
 
     public static boolean numberSize(int min, int max, int n) {
         return n <= max && n >= min;
+    }
+
+    public static boolean validateProject(String name, String clientName, String clientEmail, String price,
+            Date startDate, Date deadline, String progress) {
+        boolean r = true;
+        if (!InputValidation.textLength(3, 15, name)) {
+            MessageAlert.showError("Please enter a name that is 3 to 15 letters long.");
+            r = false;
+        } else if (!InputValidation.textLength(3, 25, clientName)) {
+            MessageAlert.showError("Please enter a client name that is 3 to 25 letters long.");
+            r = false;
+        } else if (!InputValidation.textLength(3, 25, clientEmail)) {
+            MessageAlert.showError("Please enter a client email that is 3 to 25 letters long.");
+            r = false;
+        } else if (!InputValidation.isNumber(price)) {
+            MessageAlert.showError("Please enter a valid number for the price.");
+            r = false;
+        } else if (startDate == null) {
+            MessageAlert.showError("Please select a date for the start date.");
+            r = false;
+        } else if (deadline == null) {
+            MessageAlert.showError("Please select a date for the deadline.");
+            r = false;
+        } else if (startDate.after(deadline)) {
+            MessageAlert.showError("Your start date is after the deadline.");
+            r = false;
+        } else if (!InputValidation.isNumber(progress) || Integer.parseInt(progress) < 1
+                || Integer.parseInt(progress) > 100) {
+            MessageAlert.showError("Please enter a valid number between 1 and 100 for the progress.");
+            r = false;
+        }
+
+        return r;
     }
 
 }
